6ba109dce1b622a92e033ea07138dc378a5ab34f
Stronciy <gorovin-ne@ya.ru>
2020-04-09 11:41:31 +0300

    v.0.0

---
+++ app/src/main/java/com/e/waycupcoffee/MainActivity.java
@@ -0,0 +1,14 @@
+package com.e.waycupcoffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

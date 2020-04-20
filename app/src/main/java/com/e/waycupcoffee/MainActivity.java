7db2e18e03cb9474daa47b8fddf7d285894c815f
Stronciy <gorovin-ne@ya.ru>
2020-04-20 12:07:33 +0300

    v.0.5 Убрал кнопку "О программе" Поменял логотип в хэдере на ImageButton (вызывает активити "about")

--- app/src/main/java/com/e/waycupcoffee/MainActivity.java
+++ app/src/main/java/com/e/waycupcoffee/MainActivity.java
@@ -4,13 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnMenu;
-    Button btnAbout;
+    ImageButton btnAbout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         btnMenu = (Button) findViewById(R.id.btn_menu);
         btnMenu.setOnClickListener(this);
-        btnAbout = (Button) findViewById(R.id.btn_about);
+        btnAbout = (ImageButton) findViewById(R.id.ibtn_waycup_header);
         btnAbout.setOnClickListener(this);
     }
 
@@ -29,7 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent intent = new Intent(MainActivity.this, MenuActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.btn_about:
+            case R.id.ibtn_waycup_header:
                 Intent intent2 = new Intent(MainActivity.this, AboutActivity.class);
                 startActivity(intent2);
                 break;
